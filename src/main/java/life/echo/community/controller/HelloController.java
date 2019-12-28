593ab5097a5177c073e6c15a322f628f37295e26
echo233 <936560751@qq.com>
2019-12-28 11:38:48 +0800

    init

--- src/main/java/life/echo/community/controller/HelloController.java
+++ src/main/java/life/echo/community/controller/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam(name="name")String name, Model model){
+        System.out.println("666");
         model.addAttribute("name",name);
         return "hello";
     }
